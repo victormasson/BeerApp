210b11055117ce01b83261375881f98b04fc120c
victormasson <victor.massonv@gmail.com>
2020-10-11 10:47:05 +0200

    Init project - API - Front

---
+++ Back/BerrAPI/BerrAPI/Controllers/BeersController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BerrAPI.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BerrAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BeersController : ControllerBase
+    {
+        private readonly IBeerService beerService;
+
+        public BeersController(IBeerService beerService)
+        {
+
+        }
+
+        //[HttpGet]
+        //public IList<>
+    }
+}
---
+++ Back/BerrAPI/BerrAPI/Services/BeerService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BerrAPI.Services
+{
+    public interface IBeerService
+    {
+    }
+
+    public class BeerService : IBeerService
+    {
+    }
+}
